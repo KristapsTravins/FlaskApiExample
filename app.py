dd5447e5f6d71696ee734b3c4c2ace33a29759db
Pc <kristaptravinsshop@aol.com>
2021-10-17 19:28:50 +0300

    edibo flask Mysql customers

--- app.py
+++ app.py
@@ -1,22 +1,31 @@
-from flask import Flask, request, render_template
-from markupsafe import escape
+import json
+
+from flask import Flask, request, render_template, jsonify
+from flask_cors import CORS
+from connection import DatabaseConn
+con = DatabaseConn()
 
 app = Flask(__name__)
+CORS(app)
 
 
-# created  instance
+@app.route("/customer/<string:name>/<string:lname>", methods=['GET', 'POST', 'DELETE'])
+def handle_person(name, lname):
+    if request.method == 'POST':  # we can understand what type of request we are handling using a conditional
+        con.addCustomer(name, lname, request.args.get('company'), request.args.get('email'), request.args.get('adress'), request.args.get('city'), request.args.get('country'), request.args.get('postal'), request.args.get('phone'))
+        return 'success adding customer'
+    elif request.method == 'DELETE':
+        con.deleteCustomer(name, lname)
+        return 'Deleted ' + name + " " + lname
+    else:
+        return jsonify(con.getCustInfo(name, lname))
 
-@app.route('/')
-@app.route('/hello/<name>')
-def hello():
-    return render_template('index.html')
 
+@app.route("/customers/")
+def getAllCustomers():
+    return jsonify( con.getAllCustomers())
 
-@app.errorhandler(404)
-def error(error):
-    return render_template('404.html'), 404
 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
-
---
+++ connection.py
@@ -0,0 +1,83 @@
+import pymysql
+conn = pymysql.connect(user='root', passwd='###########', db='db')
+cur = conn.cursor()
+
+
+class DatabaseConn:
+    def getCustInvoice(self, name, surname):
+        cur.execute("call db.get_invoice('" + name + "', '" + surname + "');")
+        conn.commit()
+        Elist = []
+        for col in cur.fetchall():
+            invoiceDic = {
+                "id": int(col[0]),
+                "customer_id": int(col[1]),
+                "invoice_date": col[2],
+                "billing_adress": col[3],
+                "billing_city": col[4],
+                "billing_state": col[5],
+                "billing_country": col[6],
+                "billing_postal": col[7],
+                "total": int(col[8])
+
+            }
+            Elist.append(invoiceDic)
+
+        return Elist
+
+    def getCustInfo(self, name, surname):
+        cur.execute("call db.get_customer_info('" + name + "', '" + surname + "');")
+        conn.commit()
+        Elist = []
+        for col in cur.fetchall():
+            cust_info = {
+                "id": int(col[0]),
+                "FirstName": col[1],
+                "LastName": col[2],
+                "Company": col[3],
+                "Adress": col[4],
+                "City": col[5],
+                "State": col[6],
+                "Country": col[7],
+                "PostalCode": col[8],
+                "Email": col[9],
+                "SupportRank": col[10]
+
+            }
+            Elist.append(cust_info)
+        return Elist
+
+    def addCustomer(self, name, lname, company, email, adress, city, country, postal, phone):
+        cur.execute("call db.add_customer('" + name + "','" + lname + "','" + company +"','"+adress+"', '"+city+"', '"+country+"', '"+postal+"','"+phone+"','" +email+"','2');")
+        conn.commit()
+
+    def deleteCustomer(self, name, lname):
+        cur.execute("call db.delete_custromers('"+name+"', '"+lname+"')")
+        conn.commit()
+    def getAllCustomers(self):
+        cur.execute("call db.get_all_customers()")
+        conn.commit()
+        Elist = []
+        for col in cur.fetchall():
+            invoiceDic = {
+                "id": int(col[0]),
+                "FirstName": col[1],
+                "LastName": col[2],
+                "Company": col[3],
+                "Adress": col[4],
+                "City": col[5],
+                "State": col[6],
+                "Country": col[7],
+                "PostalCode": col[8],
+                "Phone": col[9],
+                "FAX": col[10],
+                "Email": col[11],
+                "S.rank": col[12]
+            }
+            Elist.append(invoiceDic)
+
+        return Elist
+
+
+
+
